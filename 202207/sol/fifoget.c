734588888684f0d7157980568e19c92551e93f5d
Federico Menegoz <menegoz.federico@gmail.com>
2024-05-23 21:16:59 +0200

    maccati

---
+++ 202207/sol/fifoget.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+#include "lib.h"
+
+int main(int argc, char * argv[]) {
+    printf("2 + 2 = %d\n", sum(2 + 2));
+    return 0;
+}
---
+++ 202207/sol/lib.h
@@ -0,0 +1,8 @@
+#ifdef LIB_H
+#define LIB_H
+
+#define ARGS 2
+#define ERR_FIFO 2
+
+int sum(int a, int b);
+#endif
