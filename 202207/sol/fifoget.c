95487dd0000519757aaf18d1b1a2f146867401c6
Federico Menegoz <menegoz.federico@gmail.com>
2024-05-24 18:17:20 +0200

    202207 with tests

--- 202207/sol/fifoget.c
+++ 202207/sol/fifoget.c
@@ -2,17 +2,31 @@
 #include <stdlib.h>
 #include <sys/stat.h>
 #include <sys/types.h>
+#include <unistd.h>
 
 #include "lib.c"
 
+char buffer[MAX_BUFFER];
+
 int main(int argc, char* argv[]) {
-    int fd;
-    int err;
-    int n = check_parse_args(argc, argv, &err, &fd);
+    int err, fd;
+    int n = check_parse_args(argc, argv, &err);
 
     if (n < 0) {
         exit(err);
     }
+    
+    fd = open_fifo(argv[1], &err, O_RDONLY);
+    if (fd == -1) {
+        exit(err);
+    }
 
-    return 0;
+    err = fifo_to_buffer(fd, &n);
+
+    for(int i = 0; i < n; i++) {
+        printf("%c\n", buffer[i]);
+    }
+
+    close(fd);
+    return err;
 }
--- 202207/sol/lib.c
+++ 202207/sol/lib.c
@@ -7,22 +7,24 @@
 #define ERR_FIFO 2
 
 #define ARGS 2
-#define MAX_N 10
-#define MIN_N 0
+#define MAX_BUFFER 10
+#define MSG_SIZE 1
 
 #define RED "\033[0;31m"
 #define DF "\033[0m"
 
+char buffer[MAX_BUFFER];
+
 int get_n(char* str) {
     int ret = atoi(str);
-    if (strspn(str, "0123456789") == strlen(str) && ret >= MIN_N &&
-        ret <= MAX_N) {
+    if (strspn(str, "0123456789") == strlen(str) && ret >= 0 &&
+        ret <= MAX_BUFFER) {
         return ret;
     }
     return -1;
 }
 
-int check_parse_args(int argc, char* argv[], int * err, int * fd) {
+int check_parse_args(int argc, char* argv[], int * err) {
     if (argc != 3) {
         printf("%sUsage: %s <path> <n>.%s\n", RED, argv[0], DF);
         *err = ERR_ARGS;
@@ -34,13 +36,41 @@ int check_parse_args(int argc, char* argv[], int * err, int * fd) {
         *err = ERR_ARGS;
         return -1;
     }
-    if ((*fd = open(argv[1], O_RDONLY)) == -1) {
+    
+    return n;
+}
+
+int open_fifo(const char * path, int * err, int flag) {
+    int fd;
+    if ((fd = open(path, flag)) == -1) {
         printf("%s", RED);fflush(stdout);
         perror("Error opening the fifo");
         printf("%s", DF); fflush(stdout);
         *err = ERR_FIFO;
         return -1;
     }
-    return n;
+    return fd;
 }
 
+int fifo_to_buffer(int fd, int * n) {
+    int err = 0;
+    for (int i = 0; i < *n && !err; i++) {
+        char c;
+        int bytes = read(fd, &c, sizeof(char));
+        if ( bytes == -1) {
+            perror("[Reader] Error reading...");
+            err = 1; 
+        } 
+        else if (bytes == 0) {
+            err = 1;
+            *n = i;
+        }
+        else {
+            buffer[i] = c;
+        }
+    }
+    return err;
+}
+int convert_from_char(char c) {
+    return c - '0';
+}
