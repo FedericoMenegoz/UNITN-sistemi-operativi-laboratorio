639982542629d353db0d3126d9f09e3c7730708f
Federico Menegoz <menegoz.federico@gmail.com>
2024-05-25 11:16:10 +0200

    202301 - Repeater

--- 202301/sol/processA.c
+++ 202301/sol/processA.c
@@ -1,8 +1,8 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <sys/types.h>
-#include <signal.h>
 #include <unistd.h>
+#include <signal.h>
 
 #define ERR_ARGS 1
 
@@ -12,7 +12,6 @@ pid_t sig1_pid = 0;
 void handler(int signu, siginfo_t * info, void * context) {
     if (signu == SIGUSR1) {
         sig1 = 1;
-        sig1_pid = info->si_pid;
     }
     if (signu == SIGUSR2) {
         sig2 = 1;
@@ -21,10 +20,12 @@ void handler(int signu, siginfo_t * info, void * context) {
 
 int main(int argc, char * argv[]) {
     if (argc != 2) {
-        printf("Usage: %s <pathToLogFile>\n", argv[0]);
+        printf("Usage: %s <pid>\n", argv[0]);
         exit(ERR_ARGS);
     }
-    printf("[%d]\n", getpid());
+
+    int pid = atoi(argv[1]);
+
     struct sigaction sa;
     sa.sa_sigaction = handler;
     sa.sa_flags = SA_SIGINFO;
@@ -38,9 +39,10 @@ int main(int argc, char * argv[]) {
     }
 
     while(1) {
+        kill(pid, SIGUSR1);
         if (sig1) {
-            kill(sig1_pid, SIGUSR1);
-            sig1 = 0;
+            printf("Recived SIGUSR1\n");
         }
+        pause();
     }
 }
