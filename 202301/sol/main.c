b5f9cbaf83883bda7e8156642b10c68c71b874d7
Federico Menegoz <menegoz.federico@gmail.com>
2024-05-25 19:57:50 +0200

    testing

--- 202301/sol/main.c
+++ 202301/sol/main.c
@@ -1,21 +1,21 @@
+#include <errno.h>
+#include <fcntl.h>
+#include <pthread.h>
+#include <signal.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <sys/types.h>
-#include <sys/stat.h>
+#include <string.h>
 #include <sys/ipc.h>
 #include <sys/msg.h>
-#include <fcntl.h>
-#include <signal.h>
+#include <sys/stat.h>
+#include <sys/types.h>
 #include <unistd.h>
-#include <pthread.h>
-#include <string.h>
-#include <errno.h>
 
 #define ERR_ARGS 1
 #define ERR_FILE 2
 #define MAX_THREADS 10
 
-#define MAX_LOG 16 // # <pid:8> -# <signu:2> 
+#define MAX_LOG 16  // # <pid:8> -# <signu:2>
 
 struct msgq {
     long type;
@@ -29,9 +29,9 @@ pthread_t thread;
 
 int fd_log = 0;
 
-static void * thread_log(void *arg) {
+static void* thread_log(void* arg) {
     sleep(3);
-    char* message = (char*) arg;
+    char* message = (char*)arg;
     if (write(fd_log, message, strlen(message)) == -1) {
         perror("writing");
     }
@@ -41,7 +41,7 @@ static void * thread_log(void *arg) {
 
 void write_to_log(pid_t sig_pid, int signo);
 
-void handler(int signu, siginfo_t * info, void * context) {
+void handler(int signu, siginfo_t* info, void* context) {
     if (signu == SIGUSR1) {
         sig1 = 1;
         sig1_pid = info->si_pid;
@@ -58,13 +58,13 @@ void handler(int signu, siginfo_t * info, void * context) {
     }
 }
 
-int main(int argc, char * argv[]) {
+int main(int argc, char* argv[]) {
     if (argc != 2) {
         printf("Usage: %s <pathToLogFile>\n", argv[0]);
         exit(ERR_ARGS);
     }
     printf("[%d]\n", getpid());
-    
+
     struct sigaction sa;
     sa.sa_sigaction = handler;
     sa.sa_flags = SA_SIGINFO;
@@ -102,7 +102,7 @@ int main(int argc, char * argv[]) {
         perror("msgget");
     }
 
-    while(1) {
+    while (1) {
         if (sig1) {
             printf("Received sig1 from %d\n", sig1_pid);
             kill(sig1_pid, SIGUSR1);
@@ -120,13 +120,14 @@ int main(int argc, char * argv[]) {
             sig2 = 0;
         }
         if (ctrlC) {
-            if (write(fd_log, "stop\n", strlen("stop\n"))==  -1) {
+            if (write(fd_log, "stop\n", strlen("stop\n")) == -1) {
                 perror("writing stop");
             }
             ctrlC = 0;
         }
-        int bytes = msgrcv(queueId, &msg_rcv, sizeof(msg_rcv.text), 0, IPC_NOWAIT); 
-        if ( bytes == -1) {
+        int bytes =
+            msgrcv(queueId, &msg_rcv, sizeof(msg_rcv.text), 0, IPC_NOWAIT);
+        if (bytes == -1) {
             if (errno != ENOMSG) {
                 perror("msgrcv");
             }
@@ -143,12 +144,11 @@ int main(int argc, char * argv[]) {
             pthread_join(thread, NULL);
             break;
         }
-
     }
 }
 
 void write_to_log(pid_t sig_pid, int signo) {
-    char * message = (char*)malloc(MAX_LOG * sizeof(char));
+    char* message = (char*)malloc(MAX_LOG * sizeof(char));
     snprintf(message, MAX_LOG, "#%d-#%d\n", sig_pid, signo);
     if (pthread_create(&thread, NULL, thread_log, message)) {
         perror("pthred");
