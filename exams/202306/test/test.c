514987f6a98d07e2bf86e291b1af92a8d54468bf
Federico Menegoz <menegoz.federico@gmail.com>
2024-05-30 21:30:41 +0200

    Refactor

--- exams/202306/test/test.c
+++ exams/202306/test/test.c
@@ -1,16 +1,15 @@
-#include <unistd.h>
-#include <stdlib.h>
 #include <errno.h>
 #include <fcntl.h>
-#include <stdio.h>
-#include <signal.h>
-#include <string.h>
 #include <pthread.h>
-#include <sys/types.h>
+#include <signal.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 #include <sys/ipc.h>
 #include <sys/msg.h>
 #include <sys/stat.h>
-
+#include <sys/types.h>
+#include <unistd.h>
 
 #define RED "\033[0;31m"
 #define GREEN "\033[0;32m"
@@ -18,41 +17,36 @@
 #define BOLD "\033[1m"
 #define UNBOLD "\033[m"
 
-#define MAX_BUFFER 256
+#define MAX_LINE 256
+#define MAX_BUFFER 512
 #define MAX_WORD 256
 
-const char * SIG_REC = "./test/signal_receiver.out";
-const char * PROGRAM = "./sol/out/a.out";
-const char * FIFO = "./test/esame202306_fifo";
-const char * CMD_FILE = "./test/esame202306_cmd";
+const char *SIG_REC = "./test/signal_receiver.out";
+const char *PROGRAM = "./sol/out/a.out";
+const char *FIFO = "./test/esame202306_fifo";
+const char *CMD_FILE = "./test/esame202306_cmd";
 
-const char *WORDS[] = {
-    "I",
-    "won't",
-    "pass",
-    "operating",
-    "system",
-    "lab",
-    "exam!"
-};
+const char *WORDS[] = {"I",      "won't", "pass", "operating",
+                       "system", "lab",   "exam!"};
 
 void success(char *msg) {
-    printf("%s%s%s SUCCESS ! %s%s\n",BOLD, msg, GREEN, UNBOLD, DF);
+    printf("%s%s%s SUCCESS ! %s%s\n", BOLD, msg, GREEN, UNBOLD, DF);
 }
 
 void fail(char *msg) {
-    printf("%s%s%s FAILURE !%s%s\n",BOLD, msg, RED, UNBOLD, DF);
+    printf("%s%s%s FAILURE !%s%s\n", BOLD, msg, RED, UNBOLD, DF);
 }
 
-int queue_fail = 0, fifo_fail = 0, signal_receiver_pid, program_pid, sig1_flag, sig2_flag;
+int queue_fail = 0, fifo_fail = 0, signal_receiver_pid, program_pid, sig1_flag,
+    sig2_flag;
 
-
-struct msg {
+struct msg_buf {
     long category;
     char text[MAX_WORD];
 } msg_rcv;
 
-void handler(int signu, siginfo_t * info, __attribute__((unused)) void* ucontext) {
+void handler(int signu, siginfo_t *info,
+             __attribute__((unused)) void *ucontext) {
     if (signu == SIGUSR1) {
         if (info->si_pid == signal_receiver_pid) {
             sig1_flag = 1;
@@ -88,15 +82,17 @@ void *queue_reader_routine(__attribute__((unused)) void *args) {
         }
 
         if (strcmp(msg_rcv.text, WORDS[i]) != 0) {
-            snprintf(msg, MAX_BUFFER * 2, "Queue msg %s != %s", msg_rcv.text, WORDS[i]);
-            fail(msg);
-            queue_fail = 1;
-        } if (msg_rcv.category != i + 1) {
-            snprintf(msg, MAX_BUFFER, "Queue cat %ld != %d", msg_rcv.category, i + 1);
+            snprintf(msg, MAX_BUFFER, "Queue msg %s != %s", msg_rcv.text,
+                     WORDS[i]);
             fail(msg);
             queue_fail = 1;
         }
-        else {
+        if (msg_rcv.category != i + 1) {
+            snprintf(msg, MAX_LINE, "Queue cat %ld != %d", msg_rcv.category,
+                     i + 1);
+            fail(msg);
+            queue_fail = 1;
+        } else {
             kill(program_pid, SIGUSR1);
         }
     }
@@ -121,7 +117,7 @@ void *fifo_reader_routine(__attribute__((unused)) void *args) {
     char fifo_buffer[MAX_BUFFER];
     int bytes;
     int messages = sizeof(WORDS) / sizeof(char *);
-    for (int i = 0; i < messages && !fifo_fail; i ++) {
+    for (int i = 0; i < messages && !fifo_fail; i++) {
         size_t word_len = strlen(WORDS[i]) / sizeof(char);
         bytes = read(fd, fifo_buffer, word_len);
         if (bytes == -1) {
@@ -131,7 +127,8 @@ void *fifo_reader_routine(__attribute__((unused)) void *args) {
         fifo_buffer[bytes] = 0;
 
         if (strcmp(fifo_buffer, WORDS[i]) != 0) {
-            snprintf(fifo_buffer, MAX_BUFFER * 2, "Fifo msg %s != %s", msg_rcv.text, WORDS[i]);
+            snprintf(fifo_buffer, MAX_BUFFER, "Fifo msg %s != %s", msg_rcv.text,
+                     WORDS[i]);
             fail(fifo_buffer);
             fifo_fail = 1;
         } else {
@@ -151,7 +148,7 @@ int main(void) {
 
     char parent_pid[8];
     snprintf(parent_pid, 8, "%d", getpid());
-    printf("%s[%d] TEST PROGRAM%s\n",BOLD, getpid(), UNBOLD);
+    printf("%s[%d] TEST PROGRAM%s\n", BOLD, getpid(), UNBOLD);
     // Create the signal receiver process
     signal_receiver_pid = fork();
 
@@ -164,7 +161,6 @@ int main(void) {
             perror("");
             exit(2);
         }
-
     }
     struct sigaction sa;
     memset(&sa, 0, sizeof(sa));
@@ -182,34 +178,35 @@ int main(void) {
 
     if (file == -1) {
         perror("opening file");
-        exit (1);
+        exit(1);
     }
 
     // write kill sigusr1 and sigusr2 to the cmd file
-    char buffer[MAX_BUFFER];
-    snprintf(buffer, MAX_BUFFER, "kill %d %d\n", SIGUSR1, signal_receiver_pid);
+    char buffer[MAX_LINE];
+    snprintf(buffer, MAX_LINE, "kill %d %d\n", SIGUSR1, signal_receiver_pid);
     if (write(file, buffer, strlen(buffer)) == -1) {
         perror("writing to buffer");
         exit(1);
     }
-    snprintf(buffer, MAX_BUFFER, "kill %d %d\n", SIGUSR2, signal_receiver_pid);
+    snprintf(buffer, MAX_LINE, "kill %d %d\n", SIGUSR2, signal_receiver_pid);
     if (write(file, buffer, strlen(buffer)) == -1) {
         perror("writing to buffer");
         exit(1);
     }
     int messages = sizeof(WORDS) / sizeof(char *);
     for (int i = 0; i < messages; i++) {
-        snprintf(buffer, MAX_BUFFER, "queue %d %s\nfifo %s %s\n", i + 1, WORDS[i], FIFO, WORDS[i]);
+        snprintf(buffer, MAX_LINE, "queue %d %s\nfifo %s %s\n", i + 1, WORDS[i],
+                 FIFO, WORDS[i]);
         if (write(file, buffer, strlen(buffer)) == -1) {
-           perror("writing to buffer");
+            perror("writing to buffer");
             exit(1);
-        }      
+        }
     }
     close(file);
 
     // Running the solution program
     program_pid = fork();
-    if(program_pid == -1) {
+    if (program_pid == -1) {
         perror("fork");
         exit(1);
     } else if (program_pid == 0) {
@@ -219,9 +216,8 @@ int main(void) {
         }
     }
 
-
     pthread_t queue_reader, fifo_reader;
-    
+
     if (pthread_create(&queue_reader, NULL, queue_reader_routine, NULL) != 0) {
         perror("Pthread create");
         exit(1);
@@ -245,7 +241,9 @@ int main(void) {
     } else {
         fail("SIGUSR2");
     }
-    printf("%s[%d] Reading queue and fifo ... this may a take few seconds ...%s\n", BOLD, getpid(), UNBOLD);
+    printf(
+        "%s[%d] Reading queue and fifo ... this may a take few seconds ...%s\n",
+        BOLD, getpid(), UNBOLD);
     pthread_join(fifo_reader, NULL);
     pthread_join(queue_reader, NULL);
 
