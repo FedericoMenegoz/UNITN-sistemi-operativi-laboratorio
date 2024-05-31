1cb414598391c11ba4298a2d4ebe29b62c6b187a
Federico Menegoz <menegoz.federico@gmail.com>
2024-05-31 11:42:08 +0200

    202307 - 3 - handling workers signals sigusr1-2

--- exams/202307/sol/main.c
+++ exams/202307/sol/main.c
@@ -14,6 +14,22 @@
 #define BLUE "\033[0;34m"
 #define DF "\033[0m"
 
+// flags
+int sig1 = 0, sig2 = 0;
+// pids
+int sig1_pid, sig2_pid;
+
+void workers_handler(int signu, siginfo_t *info,__attribute__((unused)) void *ucontext) {
+    if(signu == SIGUSR1) {
+        sig1 = 1;
+        sig1_pid = info->si_pid;
+    }
+    if (signu == SIGUSR2) {
+        sig2 = 1;
+        sig2_pid = info->si_pid;
+    }
+}
+
 int main(int argc, char * argv[]) {
     int n;
     pid_t pid;
@@ -49,11 +65,29 @@ int main(int argc, char * argv[]) {
     printf("%s[%d] MASTER%s\n", GREEN, getpid(), DF);
     pid_t * workers = malloc(n*sizeof(pid_t));
     pid_t fork_pid;
+
+    // // Make the master ignore SIGUSR1 SIGUSR2
+    // signal(SIGUSR1, SIG_IGN);
+    // signal(SIGUSR2, SIG_IGN);
+    
+    // Creating sigaction for workers
+    struct sigaction act1;
+    act1.sa_sigaction = workers_handler;
+    act1.sa_flags = SA_SIGINFO | SA_RESETHAND;
+    sigemptyset(&act1.sa_mask);
+
+    struct sigaction act2;
+    act2.sa_flags = SA_SIGINFO;
+    act2.sa_sigaction = workers_handler;
+    sigemptyset(&act2.sa_mask);
+
     // Workers creation
     for (int i = 0; i < n; i++) {
         fork_pid = fork();
-        // If child break the loop
+        // If child add the signal handler send the signal to pid and break the loop
         if (fork_pid == 0) {
+            sigaction(SIGUSR1, &act1, NULL);
+            sigaction(SIGUSR2, &act2, NULL);
             printf("%s[%d] WORKER%s\n", BLUE, getpid(), DF);
             kill(pid, SIGTERM);
             break; 
@@ -68,6 +102,16 @@ int main(int argc, char * argv[]) {
 
     // Worker process
     while (!fork_pid) {
+        if (sig1) {
+            printf("%s[%d] Received SIGUSR1 from %d%s\n", BLUE, getpid(), sig1_pid, DF);
+            kill(sig1_pid, SIGUSR1);
+            sig1 = 0;
+        }
+        if (sig2) {
+            printf("%s[%d] Received SIGUSR2 from %d%s\n", BLUE, getpid(), sig2_pid, DF);
+            kill(sig2_pid, SIGUSR2);
+            sig2 = 0;
+        }
         pause();
     }
     // Master process
